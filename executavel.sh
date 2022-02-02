d2dc065a4b9a5422cc95f7698cc4fc46d1e0f437
rodrigocruzdev <60623579+rodrigocruzdev@users.noreply.github.com>
2022-02-02 15:31:52 -0300

    Add files via upload

---
+++ executavel.sh
@@ -0,0 +1,18 @@
+arq="$(dpkg --print-architecture)"
+arq_amd='amd'
+arq_arm='arm'
+
+sudo apt install libfuse2:$arq
+
+sudo chmod x+a ./instaladores/*.AppImage
+sudo chmod 777 ./instaladores/*.AppImage
+
+if [[ $arq == *"$arq_amd"* ]]
+   then
+    sudo ./instaladores/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage
+
+elif [[ $arq == *"$arq_arm"* ]]
+   then
+    sudo ./instaladores/install_iotedge_and_provising_device-0.1.0-armhf.AppImage
+
+fi
