9f03e32b84edeaaa7b7ed149f06b24c57629fd3e
rodrigocruzdev <60623579+rodrigocruzdev@users.noreply.github.com>
2022-02-02 15:51:12 -0300

    Update executavel.sh

--- executavel.sh
+++ executavel.sh
@@ -1,12 +1,14 @@
-arq="$(dpkg --print-architecture)"
-arq_amd='amd'
-arq_arm='arm'
-
-sudo apt install libfuse2:$arq
-
+#liberando acesso
 sudo chmod a+x ./instaladores/*.AppImage
 sudo chmod 777 ./instaladores/*.AppImage
 
+#instalando o libfuse2 para executar o .AppImage
+if !  dpkg --list | grep libfuse2  > /dev/null 2>&1
+    then
+        sudo apt install libfuse2:$arq
+fi
+
+#Executando o programa.
 if [[ $arq == *"$arq_amd"* ]]
    then
     sudo ./instaladores/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage
