b63cd1ea452f12e25d1e92bc68d130f2f2408924
rodrigocruzdev <60623579+rodrigocruzdev@users.noreply.github.com>
2022-02-02 23:02:57 -0300

    Update executavel.sh

--- executavel.sh
+++ executavel.sh
@@ -21,13 +21,13 @@ if ! ls $install_path/ | grep install_iotedge > /dev/null 2>&1
         #Executando o programa.
         if [[ $arq == *"$arq_amd"* ]]
             then
-                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage  -o $install_path/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage
+                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage  -o $install_path/install_iotedge_and_provising_device-0.1.0-x86_64.AppImage   > /dev/null 2>&1     
         elif [[ $arq == *"$arq_armhf"* ]]
             then
-                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-armhf.AppImage   -o  $install_path/install_iotedge_and_provising_device-0.1.0-armhf.AppImage
+                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-armhf.AppImage   -o  $install_path/install_iotedge_and_provising_device-0.1.0-armhf.AppImage   > /dev/null 2>&1
         elif [[ $arq == *"$arq_arm64"* ]]
             then
-                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-aarch64.AppImage -o  $install_path/install_iotedge_and_provising_device-0.1.0-aarch64.AppImage
+                curl -L https://github.com/rodrigocruzdev/instaladores/raw/main/install_iotedge_and_provising_device-0.1.0-aarch64.AppImage -o  $install_path/install_iotedge_and_provising_device-0.1.0-aarch64.AppImage  > /dev/null 2>&1
         fi
     fi
 
